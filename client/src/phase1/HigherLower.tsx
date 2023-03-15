c80a20833c99eaa7f185025813d67208922ea8bb
Arraymando <armandonoordzij@gmail.com>
2023-03-15 15:36:20 +0100

    Test

--- client/src/phase1/HigherLower.tsx
+++ client/src/phase1/HigherLower.tsx
@@ -1,5 +1,23 @@
 export const HigherLower = (props: any) => {
   const { payload } = props;
+  const {
+    cardDeck,
+    setCardDeck,
+    randomCard,
+    getRandomCard,
+    redOrBlackCorrect,
+    setRedOrBlackCorrect,
+    correctCounter,
+    setCorrectCounter,
+    gameState,
+    setGameState,
+    playerHand,
+    setPlayerHand,
+    correct,
+    setCorrect,
+  } = payload;
+
+  const higherlowerCheck = () => {};
 
   return <HigherLower />;
 };
